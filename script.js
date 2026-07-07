61a4614f2e282542770fb8c770ed3eb717af8697
Ankit-thakur1008 <ankitthakur73520@gmail.com>
2026-07-07 22:22:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+// let age =14;
+// if (age>=18){
+//     console.log("you can vote");
+// }
+// else{
+//     console.log("you cant vote");
+// }
+// let mark=41;
+// if (mark>=60){
+//     console.log("first");
+// }
+// else if(mark>=40){
+//     console.log("second");
+// }
+// else if(mark<=33){ 
+//     console.log("fail");
+// for(i=1; i<=5;i++){
+//     console.log("ankit");
+// } 
+// let sum = 0;
+// for(i=1;i<=5;i++){
+//     sum = sum +i;
+// }
+// console.log( sum)
+    
+// let string = "ankit kumar";
+// for(let i of string){
+//     console.log("i=",i);
+// // }
+// for(i=1;i<=100;i++){
+//     if(i%2 !==0)
+//     console.log("i=",i);
+// }, 30
+// let l=["geeta","sita","archana","amit","ayush","ankit"]{
+//      console.log(l)
+// let  age=14;
+// if(age>18){
+//      console.log("you can vote")
+// }else if(age<18)
+// {
+// //      console.log("you cant vote")
+// // }
+// for(i=1,i<=5,i++);
+// {
+//      console.log("i")
+// }
+// let l=[10,20,30,40,80,90]
+// l.reverse()
+// console.log(l)
+
+// function add(num1,num2){
+//      console.log(num1+num2)
+// }
+// add(20,30)
+// function username(username){
+//      console.log(`welcome home ${username}`)
+// }
+// username("amit")
+// // n number of parameter
+// let cars=["bmw,alto,scorpio,xuv500,"]
+//      console.log("cars");
+
+// for (i=1;i<=15;i=i+2){
+//      console.log(i);
+// }
+// console.log("multiplication");
+// for(i=5;i<=50;i=i+5){
+//      console.log(i);
+// }
